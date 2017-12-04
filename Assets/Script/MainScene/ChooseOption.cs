536b805b3d28368cc4e885ec908fca5c7031334c
chen <714750629@qq.com>
2017-12-04 22:42:39 +0800

    增加MainScene 场景及相关预设

---
+++ Assets/Script/MainScene/ChooseOption.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseOption : MonoBehaviour
+{
+    public List<GameObject> optionList;
+    // Use this for initialization
+    void Start()
+    {
+        optionList = new List<GameObject>();
+        InitOption();
+    }
+
+    private void InitOption()
+    {
+        var option = Resources.Load("Prefabs/Option") as GameObject;
+        var obj = Instantiate(option, this.transform);
+        var scr = obj.GetComponent<Option>();
+        scr.path = "LatticeScene";
+        optionList.Add(obj);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/MainScene/Option.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour
+{
+    public Text des;
+    public string path;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void OpenOption()
+    {
+        des.text = path;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
